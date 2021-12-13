5d0ee753a23991f127bc372d1d1cfa053bbefa49
shuntatakemoto <rt80405@gmail.com>
2021-12-13 17:21:37 +0900

    initial commit

--- course/react-typescript/src/index.tsx
+++ course/react-typescript/src/index.tsx
@@ -1,30 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { TaskList } from './components/TaskList';
 import { TaskForm } from './components/TaskForm';
+import { request } from './server';
 
 // TODOタスクの型
-export type Task = {}
+export type Task = { label: string; isDone: boolean };
 
 const App: React.VFC = () => {
   // タスクリストを格納する
-
+  const [tasks, setTasks] = useState<Task[]>([]);
   // 追加前のタスクを格納する
-
+  const [newTaskLabel, setNewTaskLabel] = useState<string>('');
   // ページマウント時にモックAPIからデータを取得
+  useEffect(() => {
+    request.fetchTasks((payload: Task[]) => setTasks(payload));
+  }, []);
 
   return (
-    <div>
+    <div style={{ width: '700px', margin: '0 auto' }}>
       {/* ヘッダー */}
       <h1>Tutorial Works</h1>
+      <h2>React Todo List</h2>
 
       {/* 一覧表示 */}
-      <TaskList />
+      <TaskList {...{ tasks, setTasks }} />
 
       {/* タスク追加、削除 */}
-      <TaskForm />
+      <TaskForm {...{ tasks, setTasks, newTaskLabel, setNewTaskLabel }} />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector('#app'));
